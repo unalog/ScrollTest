f09f67970d7b78c5c9445d569a343e94ab342e2a
una <una@ubivelox.com>
2017-04-14 18:11:41 +0900

    first commit

---
+++ ScrollTest/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ScrollTest
+//
+//  Created by una on 2017. 4. 13..
+//  Copyright © 2017년 una. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = "가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n가나다라마바사\n"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
